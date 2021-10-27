287bbd2a45e8f7db0f294998aa0c81d023458d47
Gxsary <Gxsary@163.com>
2021-10-27 22:34:09 +0800

    ci: 测试 docker 部署

--- webhooks/server.sh
+++ webhooks/server.sh
@@ -8,17 +8,16 @@ git clean -f
 echo "拉取最新代码"
 git pull origin main
 # --------------------------------------------------------
-echo "停掉 然后 开启监听"
-pwd
-npm run sprd
-npm run prd
+# echo "停掉 然后 开启监听"
+# npm run sprd
+# npm run prd
 # --------------------------------------------------------
-# echo "开始执行构建"
-# docker build -t blog-server:1.0 .
-# echo "停止并删除旧容器"
-# docker stop blog-server-container
-# docker rm blog-server-container
-# echo "启动新容器"
-# # 端口映射 宿主机 - 映射到 - 容器的3333端口
-# # -d 后台运行
-# docker container run -p 3333:3333 --name blog-server-container -d blog-server:1.0
+echo "开始执行构建"
+docker build -t blog-server:1.0 .
+echo "停止并删除旧容器"
+docker stop blog-server-container
+docker rm blog-server-container
+echo "启动新容器"
+# 端口映射 宿主机 - 映射到 - 容器的3333端口
+# -d 后台运行
+docker container run -p 3333:3333 --name blog-server-container -d blog-server:1.0
